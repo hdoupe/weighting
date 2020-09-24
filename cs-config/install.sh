634d95f58ef4e5a2b69d4e48cc238ea39f363eeb
Peter-Metz <pmetzdc@gmail.com>
2020-09-24 13:07:52 -0400

    update install script

--- cs-config/install.sh
+++ cs-config/install.sh
@@ -1,6 +1,6 @@
 # bash commands for installing your package
 
-git clone https://github.com/Peter-Metz/weighting/tree/app
+git clone -b app https://github.com/Peter-Metz/weighting
 cd weighting
 git checkout app
 conda install PSLmodels::taxcalc anaconda::scipy conda-forge::paramtools
